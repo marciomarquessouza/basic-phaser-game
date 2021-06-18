ddd6979df5072517059d64bd2904978a5fe5e372
marciomarquessouza <marciomultimedia@gmail.com>
2021-06-18 02:03:20 -0300

    test: add types to test file

--- tests/test.file.ts
+++ tests/test.file.ts
@@ -1,7 +1,7 @@
 import { NightwatchBrowser } from "nightwatch";
 
 describe("#Test Basic Phaser Game", function () {
-  test("check if body and canvas are working properly", function (browser: unknown) {
+  it("check if body and canvas are working properly", function (browser: unknown) {
     (browser as NightwatchBrowser)
       .url("http://localhost:5000/")
       .waitForElementVisible("body")
