2b7bb8ec2266e514bbc355ad5727db9f82e469b7
marciomarquessouza <marciomultimedia@gmail.com>
2021-06-17 22:49:27 -0300

    test: convert test to typescript

--- tests/test.file.ts
+++ tests/test.file.ts
@@ -1,6 +1,8 @@
+import { NightwatchBrowser } from "nightwatch";
+
 describe("#Test Basic Phaser Game", function () {
-  test("check if body and canvas are working properly", function (browser) {
-    browser
+  test("check if body and canvas are working properly", function (browser: unknown) {
+    (browser as NightwatchBrowser)
       .url("http://localhost:5000/")
       .waitForElementVisible("body")
       .waitForElementVisible("canvas")
