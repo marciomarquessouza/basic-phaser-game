729361805aa32dd978bdd2df3ea061425f0cf560
marciomarquessouza <marciomultimedia@gmail.com>
2021-06-18 21:28:48 -0300

    refactor: merge base weback with test

--- webpack.test.js
+++ webpack.test.js
@@ -1,66 +1,8 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const stylesHandler = "style-loader";
+const { merge } = require("webpack-merge");
+const base = require("./webpack.base");
 
-module.exports = {
-  mode: "development",
-  devtool: "source-map",
+module.exports = merge(base, {
   entry: {
     index: "./tests/index.ts",
   },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
-  },
-  devServer: {
-    open: true,
-    host: "localhost",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "index.html",
-    }),
-    new webpack.DefinePlugin({
-      CANVAS_RENDERER: JSON.stringify(true),
-      WEBGL_RENDERER: JSON.stringify(true),
-    }),
-    new CleanWebpackPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
-        exclude: ["/node_modules/"],
-      },
-      {
-        test: /\.css$/i,
-        use: [stylesHandler, "css-loader"],
-      },
-      {
-        test: [/\.vert$/, /\.frag$/],
-        use: "raw-loader",
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg|xml)$/i,
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-              pngquant: {
-                quality: [0.65, 0.9],
-                speed: 4,
-              },
-            },
-          },
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-};
+});
