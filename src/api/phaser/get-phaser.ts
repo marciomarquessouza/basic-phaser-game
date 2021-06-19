87bf3bf3b7ff6f33216e3337ffed1121b6423473
marciomarquessouza <marciomultimedia@gmail.com>
2021-06-19 14:18:22 -0300

    refactor: move cache to an specific file

--- src/api/phaser/get-phaser.ts
+++
@@ -1,7 +0,0 @@
-export function getPhaser(
-  config: Phaser.Types.Core.GameConfig
-): Promise<Phaser.Game> {
-  return import("./phaser-custom.js").then(({ default: Phaser }) => {
-    return new Phaser.Game(config);
-  });
-}
--- src/main/adapters/overlap-callback.protocol.ts
+++
@@ -1,1 +0,0 @@
-export type OverlapCallback = (obj1Name: string, obj2Name: string) => void;
