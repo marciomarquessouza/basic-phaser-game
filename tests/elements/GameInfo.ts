431b0e4a502eef2d801e6de837bb58f3fcf23f3c
marciomarquessouza <marciomultimedia@gmail.com>
2021-06-19 14:26:52 -0300

    test: update GameInfo component with name property

--- tests/elements/GameInfo.ts
+++ tests/elements/GameInfo.ts
@@ -1,9 +1,12 @@
-import { LitElement, html, css } from "lit";
-import { customElement } from "lit/decorators.js";
+import { LitElement, html } from "lit";
+import { customElement, property } from "lit/decorators.js";
 
 @customElement("game-info")
 export class GameInfo extends LitElement {
+  @property()
+  name = "waiting";
+
   render() {
-    return html`<h1>Example of LitElement</h1>`;
+    return html`<h1>${this.name}</h1>`;
   }
 }
