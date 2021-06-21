21d7dc90b19b7ed873eb2e1c8fed80cef7fc15aa
marciomarquessouza <marciomultimedia@gmail.com>
2021-06-21 18:01:48 -0300

    test: update nightwatch test for typescript

---
+++ tests/e2e/test.file.ts
@@ -0,0 +1,12 @@
+import { NightwatchBrowser } from "nightwatch";
+
+describe("#Test Basic Phaser Game", function () {
+  it("check if body and canvas are working properly", function (browser: unknown) {
+    (browser as NightwatchBrowser)
+      .url("http://localhost:5000/")
+      .waitForElementVisible("body")
+      .waitForElementVisible("canvas")
+      .assert.titleContains("Basic Phaser Game")
+      .end();
+  });
+});
