b29eaeeeb28514b0ec8be0a8be41ad82fb7ffe51
marciomarquessouza <marciomultimedia@gmail.com>
2021-06-21 18:02:14 -0300

    fix: add type to the config phaser

--- src/main/config/game-config.ts
+++ src/main/config/game-config.ts
@@ -1,9 +1,11 @@
+import Phaser from "phaser";
 import { GRAVITY, HEIGHT, WIDTH } from "../../constants";
 
 export const config = (
   scene: Phaser.Types.Scenes.CreateSceneFromObjectConfig
 ): Phaser.Types.Core.GameConfig => {
   return {
+    type: Phaser.AUTO,
     width: WIDTH,
     height: HEIGHT,
     physics: {
