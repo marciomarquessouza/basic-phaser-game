e5e70e10b68b0cddf249b751687e214b5fae6d24
marciomarquessouza <marciomultimedia@gmail.com>
2021-06-22 01:45:34 -0300

    test: add update status observer

--- tests/web/index.ts
+++ tests/web/index.ts
@@ -1,17 +1,13 @@
 import { config } from "../../src/main/config";
 import { getPhaser } from "../../src/api";
 import { scene01 } from "../../src/main/scenes";
-import { SceneObserver } from "../../src/main";
+import { updateStatusByObserver } from "./update-status-by-observer";
 import { playerObserver } from "../../src/main/game-data/player";
 import "./style.css";
 
-// const sceneObserver: SceneObserver = function (data) {
-//   gameInfo.setAttribute("name", `${data.value}`);
-// };
-
-// playerObserver.subscribe(sceneObserver);
-
 console.log("loding...");
+
+updateStatusByObserver(playerObserver);
 
 async function getGame() {
   const gameConfig = config(scene01);
