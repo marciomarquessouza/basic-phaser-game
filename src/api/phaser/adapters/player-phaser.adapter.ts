db4eb8875a75656f116c73791913d5140061dc33
marciomarquessouza <marciomultimedia@gmail.com>
2021-06-22 01:48:27 -0300

    feat: update player status after phaser action

--- src/api/phaser/adapters/player-phaser.adapter.ts
+++ src/api/phaser/adapters/player-phaser.adapter.ts
@@ -1,10 +1,10 @@
+import { ANIMATION_KEY } from "../../../constants";
 import {
   CreatePlayerAdapter,
   GetTouchingAdapter,
   PlayAnimationAdapter,
   SetVelocityAdapter,
   GetTouching,
-  PlayAnimation,
   SetVelocity,
 } from "../../../data";
 import { Player } from "../../../entities";
@@ -20,29 +20,49 @@ export class PlayerPhaser
   create(player: Player) {
     playerHelper
       .createPlayer(player)
-      .then(() => playerHelper.createAnimation(player))
-      .finally(() => {
+      .then(() => {
+        playerHelper.createAnimation(player);
         player.status = "created";
+      })
+      .catch(() => {
+        player.status = "error";
       });
   }
 
-  play(animation: PlayAnimation.Params): void {
-    const { player, key, ignoreIfPlaying } = animation;
-    playerHelper.playAnimation(player.name, key, ignoreIfPlaying);
+  play(player: Player, key: ANIMATION_KEY, ignoreIfPlaying?: boolean): void {
+    playerHelper
+      .playAnimation(player.name, key, ignoreIfPlaying)
+      .then(() => {
+        player.key = key;
+        player.ignoreIfPlaying = ignoreIfPlaying;
+      })
+      .catch(() => {
+        player.status = "error";
+      });
   }
 
   setVelocity(playerVelocity: SetVelocity.Params): void {
     const { player, velocity } = playerVelocity;
     const { name } = player;
-    if (!isNaN(velocity.x)) {
-      playerHelper.setVelocityX(name, velocity.x);
-    }
-    if (!isNaN(velocity.y)) {
-      playerHelper.setVelocityY(name, velocity.y);
+    try {
+      if (!isNaN(velocity.x)) {
+        playerHelper.setVelocityX(name, velocity.x).then(() => {
+          player.velocityX = velocity.x;
+        });
+      }
+      if (!isNaN(velocity.y)) {
+        playerHelper.setVelocityY(name, velocity.y).then(() => {
+          player.velocityY = velocity.y;
+        });
+      }
+    } catch (error) {
+      player.status = "error";
     }
   }
 
   getTouching(player: Player): GetTouching.Result {
-    return playerHelper.getTouching(player.name);
+    const touching = playerHelper.getTouching(player.name);
+    Object.assign(player, touching);
+    return touching;
   }
 }
